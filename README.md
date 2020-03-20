5572748b8276e19cc0150a16bffe5ed44fd35bee
Raymond Jacobson <ray@audius.co>
2020-03-20 14:04:32 -0700

    Get everything working

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# cartographer
-Audius sitemap generator
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/2731362/64272626-9c3b8800-cef4-11e9-973e-0e38a666ec46.png" alt="stems" width="340">
+
+  <p align="center">
+    <a href="https://audius.co/sitemaps/index.xml">audius.co</a> sitemap generator
+  </p>
+</p>
+
+<br/>
+<br/>
+
+## Run
+
+The Cartographer sitemap generator is a node script that runs and is stateful on the presence of a local /sitemap directory.
+
+Given the existence of sitemaps in the `sitemaps/` directory and the `sitemaps/` latest.yaml config, Cartographer will crawl an Audius discovery provider, indexing new entries into the `/sitemaps` folder, which can then be persisted and served.
+
+Example usage:
+```bash
+# Pull the staging sitemaps down
+npm run pull:staging
+
+# Run Cartographer
+#  count: maximum number of each tracks, collections, and users fetch
+npm run start:staging -- --count=100
+
+# Sync your changes to staging
+npm run sync:staging
+```
+
+The same series of commands can be run with :production instead of staging.
+
+Be careful when doing that!
+
+You can also wipe your local `sitemaps/` folder with
+
+```
+npm run reset
+```
+
+### Notes
+
+> In Cartographer's current form, it only crawls forward and doesn't re-index things it has already re-indexed. To reindex the entirety of audius, reset and regenerate the entire sitemap.
+
+> Unfortunately, Cartographer doesn't know good "stopping" criteria of whether or not a route is simply unavailable (unpublished playlist / hidden track) and whether that would mean to stop indexing. Cartographer accepts a maximum of 10 consecutive failed requests before it gives up on fetching more. This can be configured with the `--fails=N` flag in `npm run start`
+
+## CI
+
+The sitemap generator runs in circleci every hour with --count=100
+
+That config can be found in ./circleci/config.yml
+
+## Deployment
+
+The sitemap generator outputs the following directory structure
+
+```
+sitemaps
+│   index.xml
+|
+└───tracks
+│   │   index.xml
+│   │   1.xml  // Max 50K entries
+|   |   2.xml
+|   |   ...
+└───collections
+│   │   index.xml
+│   │   1.xml
+|   |   2.xml
+|   |   ...
+└───users
+│   │   index.xml
+│   │   1.xml
+|   |   2.xml
+|   |   ...
+```
+
+Depending on the crawler, the root index.xml file may not be able to be provided (as nested indexes aren't supported) and robots.txt files should list each index file for tracks, collections, and users.
---
+++ index.js
@@ -0,0 +1,158 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const { fetchTracks, fetchCollections, fetchUsers } = require('./lib/queries')
+const {
+  getLatestSiteMapNumber,
+  createRootSiteIndex,
+  createOrAppendSiteMap,
+  createOrAppendSiteIndex
+} = require('./lib/sitemap')
+
+const argv = require('minimist')(process.argv.slice(2))
+console.log('Invoked with args: ', argv)
+
+const ROOT = './sitemaps'
+const INDEX = './sitemaps/index.xml'
+const TRACKS_INDEX = './sitemaps/tracks/index.xml'
+const COLLECTIONS_INDEX = './sitemaps/collections/index.xml'
+const USERS_INDEX = './sitemaps/users/index.xml'
+
+const TRACKS_SITE_MAP_ROOT = './sitemaps/tracks'
+const COLLECTIONS_SITE_MAP_ROOT = './sitemaps/collections'
+const USERS_SITE_MAP_ROOT = './sitemaps/users'
+
+const LATEST = './sitemaps/latest.yml'
+
+const makeDirs = () => {
+  if (!fs.existsSync(ROOT)) {
+    fs.mkdirSync(ROOT)
+  }
+  if (!fs.existsSync(TRACKS_SITE_MAP_ROOT)) {
+    fs.mkdirSync(TRACKS_SITE_MAP_ROOT)
+  }
+  if (!fs.existsSync(COLLECTIONS_SITE_MAP_ROOT)) {
+    fs.mkdirSync(COLLECTIONS_SITE_MAP_ROOT)
+  }
+  if (!fs.existsSync(USERS_SITE_MAP_ROOT)) {
+    fs.mkdirSync(USERS_SITE_MAP_ROOT)
+  }
+}
+
+// Read stateful config to help us know where to index from
+const readLatest = () => {
+  let ret
+  try {
+    const { latest } = yaml.safeLoad(
+      fs.readFileSync(LATEST, 'utf8')
+    )
+    ret = {
+      latestTrack: latest.track,
+      latestCollection: latest.collection,
+      latestUser: latest.user
+    }
+  } catch (e) {
+    ret = {
+      latestTrack: 0,
+      latestCollection: 0,
+      latestUser: 0
+    }
+  }
+  console.log('\nLatest: ', ret, '\n')
+  return ret
+}
+
+// Updates the stateful config
+const updateLatest = (latestTrack, latestCollection, latestUser) => {
+  const obj = {
+    latest: {
+      track: latestTrack,
+      collection: latestCollection,
+      user: latestUser
+    }
+  }
+  console.log('\nUpdated Latest: ', obj.latest, '\n')
+  const y = yaml.safeDump(obj)
+  fs.writeFileSync(LATEST, y)
+}
+
+/**
+ * Indexes audius and creates/appends to sitemaps in the ./sitemaps folder.
+ * This script is intended to be run as a cron-job and keep state of what it
+ * has already indexed.
+ */
+const run = async () => {
+  try {
+    makeDirs()
+
+    // Create a master site index if needed
+    createRootSiteIndex(INDEX, TRACKS_INDEX, COLLECTIONS_INDEX, USERS_INDEX)
+
+    // Read from the latest.yaml config to determine where to start indexing
+    const { latestTrack, latestCollection, latestUser } = readLatest()
+
+    // Fetch tracks, collections, and users for processing
+    const [fetchedTracks, fetchedCollections, fetchedUsers] = await Promise.all([
+      fetchTracks(latestTrack, argv.count, argv.fails),
+      fetchCollections(latestCollection, argv.count, argv.fails),
+      fetchUsers(latestUser, argv.count, argv.fails)
+    ])
+    const { items: tracks, latest: newLatestTrack } = fetchedTracks
+    const { items: collections, latest: newLatestCollection } = fetchedCollections
+    const { items: users, latest: newLatestUser } = fetchedUsers
+
+    console.log('\n')
+    console.log(`Tracks: Fetched ${newLatestTrack - latestTrack} new`)
+    console.log(`Collections: Fetched ${newLatestCollection - latestCollection} new`)
+    console.log(`Users: Fetched ${newLatestUser - latestUser} new`)
+    console.log('\n')
+
+    // Create or append to sitemaps (potentially generating new ones because
+    // the maximum size of a sitemap is 50K entrires)
+    const latestTrackNumber = await createOrAppendSiteMap(
+      tracks,
+      getLatestSiteMapNumber(latestTrack),
+      TRACKS_SITE_MAP_ROOT
+    )
+    const latestCollectionNumber = await createOrAppendSiteMap(
+      collections,
+      getLatestSiteMapNumber(latestCollection),
+      COLLECTIONS_SITE_MAP_ROOT
+    )
+    const latestUserNumber = await createOrAppendSiteMap(
+      users,
+      getLatestSiteMapNumber(latestUser),
+      USERS_SITE_MAP_ROOT
+    )
+
+    // Create or append to sub-indexes
+    // Note: These may need to be root indexes for most crawlers as
+    // nesting isn't always supported
+    await createOrAppendSiteIndex(
+      TRACKS_INDEX,
+      TRACKS_SITE_MAP_ROOT,
+      getLatestSiteMapNumber(latestTrack),
+      latestTrackNumber
+    )
+    await createOrAppendSiteIndex(
+      COLLECTIONS_INDEX,
+      COLLECTIONS_SITE_MAP_ROOT,
+      getLatestSiteMapNumber(latestCollection),
+      latestCollectionNumber
+    )
+    await createOrAppendSiteIndex(
+      USERS_INDEX,
+      USERS_SITE_MAP_ROOT,
+      getLatestSiteMapNumber(latestUser),
+      latestUserNumber
+    )
+
+    // Update the latest.yml file that keeps track of what we've indexed
+    updateLatest(newLatestTrack, newLatestCollection, newLatestUser)
+  } catch (e) {
+    console.error('Some error occured, output may be corrupted')
+    console.error(e)
+  }
+}
+
+run()
